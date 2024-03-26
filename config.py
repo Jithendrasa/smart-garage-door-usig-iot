88269ec124066c405143315b59989cbc8709c9fa
Jithendrasa <129645594+Jithendrasa@users.noreply.github.com>
2024-03-26 11:58:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# conf.py
+API_KEY = "your_bolt_api_key"
+DEVICE_ID = "your_bolt_device_id"
+SID = "your_twilio_sid"
+AUTH_TOKEN = "your_twilio_auth_token"
+TO_NUMBER = "your_destination_phone_number"
+FROM_NUMBER = "your_twilio_phone_number"
